98a7a39c36bb33870532e173cbfdabbc04153f9c
Lee Mingi <minki319@naver.com>
2021-10-15 18:09:22 +0900

    test!!
    이미지를 약간 어둡게 하고 디텍팅 추가

--- detect.py
+++ detect.py
@@ -8,15 +8,18 @@ from multiprocessing import Pool
 import multiprocessing
 
 class detectClass(object):
-    screenRate = 0.3
-    detectPadding = 5
+    screenRate = 0.9
+    detectPadding = 10
 
     def __init__(self):
         self.arr = multiprocessing.Manager().list()
+        self.boxarr = multiprocessing.Manager().list()
 
     def detect(self, downimage):
         #image = cv2.resize(image, dsize=(1000, 1000), interpolation=cv2.INTER_AREA)
-        ImportImage = cv2.resize(downimage, dsize=(0, 0), fx=self.screenRate, fy=self.screenRate, interpolation=cv2.INTER_LINEAR)
+        FirstImage = cv2.resize(downimage, dsize=(0, 0), fx=self.screenRate, fy=self.screenRate, interpolation=cv2.INTER_LINEAR)
+        ImportImage = FirstImage - 50
+
         gray = cv2.cvtColor(ImportImage, cv2.COLOR_BGR2GRAY)
 
         #cv2.imwrite('testImg/gray.jpg', gray)
@@ -32,7 +35,7 @@ class detectClass(object):
 
         #cv2.imwrite('testImg/blurred.jpg', blurred)
 
-        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (21, 7))
+        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (40, 20))
         closed = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
 
         cv2.imwrite('testImg/closed.jpg', closed)
@@ -45,6 +48,7 @@ class detectClass(object):
 
         b,g,r,a = 0,0,255,0
 
+        #각각의 영역 좌표를 가져오고 값을 location에 저장
         location = []
         for i in c:
             rect = cv2.minAreaRect(i)
@@ -55,42 +59,72 @@ class detectClass(object):
             y2 = y + h + int(self.detectPadding)
             x2 = x + w + int(self.detectPadding)
             location += [[y1,y2,x1,x2]]
+            self.boxarr += [[box]]
+            # 영역 표시를 위해
             # cv2.drawContours(ImportImage, [box], -1, (0, 255, 0), 1)
 
+        #저장된 좌표값을 넘겨줘서 바코드 decode
         print("multi process start")
-        print(len(location))
         # print(location[:len(location)])
-        box1 = location[:len(location)//2]
-        box2 = location[len(location)//2:]
-        process1 = multiprocessing.Process(target=self.drawLine, args=[gray, box1])
-        process2 = multiprocessing.Process(target=self.drawLine, args=[gray, box2])
-        process1.start()
-        process2.start()
-        process1.join()
-        process2.join()
-            # drawLine(c)
+        processNum = 5
+        locationLen = len(location)
+        subLocation = len(location) // processNum
+        procs = []
+
+        count = 0
+        for number in range(processNum):
+            start = count * subLocation
+            end = (count+1) * subLocation
+            if(end > locationLen): end = locationLen
+            proc = multiprocessing.Process(target=self.drawLine, args=[FirstImage, location[start:end]])
+            procs.append(proc) 
+            proc.start()
+            count += 1
+        for proc in procs:
+            proc.join()
+
+
+        print("검출 갯수 : " + str(len(self.arr)))
+        print("바코드 : " + str(self.arr))
+
+        #xy1 = location[:len(location)//2]
+        #xy2 = location[len(location)//2:]
+        #process1 = multiprocessing.Process(target=self.drawLine, args=[FirstImage, xy1])
+        #process2 = multiprocessing.Process(target=self.drawLine, args=[FirstImage, xy2])
+        #process1.start()
+        #process2.start()
+        #process1.join()
+        #process2.join()
+
+        #print(self.arr)
+
+        print("multi process end")
+        
+        for i in self.boxarr:
+            cv2.drawContours(ImportImage, i, -1, (0, 255, 0), 1)
 
         return ImportImage 
 
     def drawLine(self, image, location):
         count = 0
         for i in location:
-            gettime = time.time()
+            #gettime = time.time()
             barcode = decode(image[i[0]:i[1], i[2]:i[3]])
-            cv2.imwrite('testImg/' + str(gettime) + '.jpg', image[i[0]:i[1], i[2]:i[3]])
+            #cv2.imwrite('testImg/' + str(gettime) + '.jpg', image[i[0]:i[1], i[2]:i[3]])
             count += 1
-            if barcode != []: self.arr.append(barcode)
+            if barcode != []: 
+                self.arr.append(barcode)
 
 
     def Image(self):
-        image = cv2.imread('sample/barcode3.jpg')
+        image = cv2.imread('sample/webcam2.jpg')
+        start_time = time.time()
         showImg = self.detect(image)
-        # cv2.imshow('',showImg)
-        # key = cv2.waitKey(1)
+        end_time = time.time()
+        print(end_time - start_time)
+        cv2.imshow('',showImg)
+        key = cv2.waitKey(1)
         cv2.imwrite('testImg/0.jpg', showImg)
-        print(self.arr)
-        print(len(self.arr))
-
 
     def webcam(self):
         cap = cv2.VideoCapture(0)
@@ -123,10 +157,8 @@ class detectClass(object):
 
 
 if __name__ == "__main__":
-    start_time = time.time()
     a = detectClass()
     a.Image()
     # a.webcam() 
     # a.webcamOnce()
-    end_time = time.time()
-    print(end_time - start_time)
+    #print(end_time - start_time)
